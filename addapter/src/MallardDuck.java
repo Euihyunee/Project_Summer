4ddbf4c477eee06aca59cb96a808d6e6dc91b78e
euihyuneee <lobil7782@gmail.com>
2022-07-10 12:03:11 +0900

    [ADD] 어댑터 패턴, 옵저버 패턴 추가

---
+++ addapter/src/MallardDuck.java
@@ -0,0 +1,10 @@
+public class MallardDuck implements Duck{
+    @Override
+    public void quack(){
+        System.out.println("Quack");
+    }
+    @Override
+    public void fly(){
+        System.out.println("I'm flying");
+    }
+}
---
+++ addapter/src/WildTurkey.java
@@ -0,0 +1,10 @@
+public class WildTurkey implements Turkey{
+    @Override
+    public void gobble(){
+        System.out.println("Goobble gobble");
+    }
+    @Override
+    public void fly(){
+        System.out.println("I'm flying a short distance");
+    }
+}

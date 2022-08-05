8eb4a03e72b482f1540b789b0a2c18155c87f6eb
euihyuneee <lobil7782@gmail.com>
2022-08-05 17:46:01 +0900

    [ADD] CRUD게시판 만들기

--- facade/src/System03.java
+++ facade/src/System03.java
@@ -1,8 +1,10 @@
 class System03 {
     public System03() {
+
         System.out.println("Call Constructor : " + getClass().getSimpleName());
     }
     public void process(){
+
         System.out.println("Call Process : " +getClass().getSimpleName());
     }
 }

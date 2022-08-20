8641fa792b6ab23df4022209c8791fa7e3d3fd4f
euihyuneee <lobil7782@gmail.com>
2022-08-21 05:46:45 +0900

    [ADD] 프록시 패턴

---
+++ proxy/src/Image.java
@@ -0,0 +1,5 @@
+// subject
+// 로딩이 먼저 끝난 텍스트를 먼저 나오게 하기
+public interface Image {
+    public void displayImage();
+}
---
+++ proxy/src/Real_Image.java
@@ -0,0 +1,17 @@
+public class Real_Image implements Image {
+    private String fileName;
+
+    // 생성자
+    public Real_Image(String fileName) {
+        this.fileName = fileName;
+    }
+
+    private void loadFromDisk(String fileName) {
+        System.out.println("로딩: " + fileName);
+    }
+
+    @Override
+    public void displayImage() {
+        System.out.println("보여주기: " + fileName);
+    }
+}

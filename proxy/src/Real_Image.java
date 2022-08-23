b1b6e61d8e8dc11b76aa567904352bcc2b37ee76
euihyuneee <lobil7782@gmail.com>
2022-08-24 05:36:11 +0900

    [UPDATE] 패턴 설명 추가

--- proxy/src/Real_Image.java
+++ proxy/src/Real_Image.java
@@ -7,11 +7,13 @@ public class Real_Image implements Image {
     }
 
     private void loadFromDisk(String fileName) {
+
         System.out.println("로딩: " + fileName);
     }
 
     @Override
     public void displayImage() {
+
         System.out.println("보여주기: " + fileName);
     }
 }
